4c070df3c4b77db01a0afc69e680134caf601883
Hans Niklas Jacob <hnj@posteo.de>
2020-01-05 22:22:16 +0100

    added templates

---
+++ html/apply.sh
@@ -0,0 +1,13 @@
+(
+cat header.html.tl;
+cat forderungen-kurz.html.tl;
+cat unterstuetzer.html.tl;
+cat footer.html.tl
+) >index.html
+
+(
+cat header.html.tl;
+cat forderungen-lang.html.tl;
+cat unterstuetzer.html.tl;
+cat footer.html.tl
+) >aufruf.html
