6abbf564eef720844e6fa9eb1aa11d5ce9cffa28
Hans Niklas Jacob <hnj@posteo.de>
2020-01-06 02:08:30 +0100

    final version:)

--- html/apply.sh
+++ html/apply.sh
@@ -2,6 +2,8 @@
 cat header.html;
 cat title-large.html;
 cat forderungen-kurz.html;
+cat testimonials.html;
+cat argumente-kurz.html;
 cat unterstuetzer.html;
 cat footer.html
 ) >index.html
@@ -11,6 +13,8 @@ cat header.html;
 cat title-small.html;
 cat forderungen-lang.html;
 cat unterstuetzer.html;
+cat minusspace.html;
+cat testimonials.html;
 cat footer.html
 ) >aufruf.html
 
