c7ebc126326af3ea634c4d52d04823bf8c624488
Hans Niklas Jacob <hnj@posteo.de>
2020-01-10 01:41:45 +0100

    Updated to webstie state

--- html/apply.sh
+++ html/apply.sh
@@ -18,4 +18,24 @@ cat testimonials.html;
 cat footer.html
 ) >aufruf.html
 
-zip ../html.zip -xr index.html aufruf.html datenschutz.html impressum.html css/styles.css img/* img/gesichter/*
+(
+cat header.html;
+cat title-small.html;
+cat pressemitteilung.html;
+cat testimonials.html;
+cat minusspace.html;
+cat unterstuetzer.html;
+cat footer.html
+) >pm.html
+
+(
+cat header.html;
+cat was-kann-ich-tun.html;
+cat testimonials.html;
+cat minusspace.html;
+cat unterstuetzer.html;
+cat footer.html
+) >action.html
+
+
+zip ../html.zip -xr index.html aufruf.html action.html pm.html datenschutz.html impressum.html css/styles.css img/* img/gesichter/*
