425da78239674ed45031c6f828a7580b533bf9b3
vik <vik>
2020-01-11 16:41:56 +0100

    falsk basics

---
+++ newsletter/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World from Uberspace and Flask!"
+
+if __name__ == "__main__":
+    app.run()
