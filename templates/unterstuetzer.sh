2f28675da8dd78971ec419d11f6cdff7bccf3479
Hans Niklas Jacob <hnj@posteo.de>
2020-01-14 15:18:33 +0100

    Fixed Load stuff

--- templates/unterstuetzer.sh
+++ templates/unterstuetzer.sh
@@ -36,7 +36,7 @@ EOF
 make_box ccc.jpg "Chaos Computer Club e.V." "https:\/\/ccc.de"
 make_box dg.jpg  "Digitale Gesellschaft e.V." "https:\/\/digitalegesellschaft.de"
 make_box d64.svg  "D64 Zentrum für Digitalen Fortschritt e.V." "https:\/\/d-64.org"
-make_box load.png  "LOAD e.V. Verein für liberale Netzpolitik" "https:\/\/www.load-ev.de/"
+make_box load.png  "LOAD e.V. Verein für liberale Netzpolitik" "https:\/\/www.load-ev.de"
 fiff_long="Forum InformatikerInnen für Frieden und gesellschaftliche Verantwortung e.V."
 make_box fiff.jpg "<span class=\"is-hidden-tablet\">$fiff_long<\/span><span class=\"is-hidden-mobile\">FIfF e.V.<\/span>" "https:\/\/fiff.de"
 make_box df.svg "Digitale Freiheit e.V." "https:\/\/digitale-freiheit.jetzt"
