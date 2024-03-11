2bf7efb2dc68177afce10d70b3674a633d44ea4d
Hans Niklas Jacob <hnj@posteo.de>
2024-03-11 23:29:44 +0100

    Updated Logos

--- templates/unterstuetzer.sh
+++ templates/unterstuetzer.sh
@@ -34,20 +34,21 @@ EOF
 }
 
 make_box ccc.jpg "Chaos Computer Club e.V." "https:\/\/ccc.de"
+make_box aw.png "AlgorithmWatch" "https:\/\/algorithmwatch.org"
 make_box ai.png  "Amnesty International e.V." "https:\/\/www.amnesty.de"
 make_box dg.jpg  "Digitale Gesellschaft e.V." "https:\/\/digitalegesellschaft.de"
-make_box d64.svg  "D64 Zentrum für Digitalen Fortschritt e.V." "https:\/\/d-64.org"
+make_box d64.png  "D64 Zentrum für Digitalen Fortschritt e.V." "https:\/\/d-64.org"
 make_box ryf.png "ReclaimYourFace" "https:\/\/reclaimyourface.eu"
 make_box load.png  "LOAD e.V. Verein für liberale Netzpolitik" "https:\/\/www.load-ev.de"
 fiff_long="Forum InformatikerInnen für Frieden und gesellschaftliche Verantwortung e.V."
-make_box fiff.jpg "<span class=\"is-hidden-tablet\">$fiff_long<\/span><span class=\"is-hidden-mobile\">FIfF e.V.<\/span>" "https:\/\/fiff.de"
+#make_box fiff.jpg "<span class=\"is-hidden-tablet\">$fiff_long<\/span><span class=\"is-hidden-mobile\">FIfF e.V.<\/span>" "https:\/\/fiff.de"
 make_box df.svg "Digitale Freiheit e.V." "https:\/\/digitale-freiheit.jetzt"
-make_box dc.png "Digitalcourage e.V." "https:\/\/digitalcourage.de"
-make_box hu.jpg "Humanistische Union e.V." "https:\/\/www.humanistische-union.de"
-make_box sd.png "Selbstbestimmt.Digital e.V." "https:\/\/www.selbstbestimmt-digital.de"
+#make_box dc.png "Digitalcourage e.V." "https:\/\/digitalcourage.de"
+#make_box hu.jpg "Humanistische Union e.V." "https:\/\/www.humanistische-union.de"
+#make_box sd.png "Selbstbestimmt.Digital e.V." "https:\/\/www.selbstbestimmt-digital.de"
 make_box topio.jpg "Topio \/\/\/ public space for privacy" "https:\/\/www.topio.info"
-make_box gw.png "Germanwatch" "https:\/\/germanwatch.org"
-make_box gi.svg "Gesellschaft für Informatik" "https:\/\/gi.de"
+#make_box gw.png "Germanwatch" "https:\/\/germanwatch.org"
+#make_box gi.svg "Gesellschaft für Informatik" "https:\/\/gi.de"
 
 
 
